b7f984e3e221b1b805c944ab156bb886031352b7
Max Chen <ckyb63@gmail.com>
2025-02-07 21:24:56 -0500

    ROS2 Package Fix

--- setup.py
+++ setup.py
@@ -1,27 +1,33 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'gui_package'
 
 setup(
     name=package_name,
-    version='0.3.0',
+    version='0.3.1',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
+        # Explicitly specify the marker file installation
+        ('share/ament_index/resource_index/packages', 
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Add any launch files if you have them
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Max Chen',
     maintainer_email='ckyb63@gmail.com',
-    description='PyQt5-based GUI for PPE Vending Machine with ROS2 integration',
+    description='GUI package for PPE vending machine',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'ppe_gui = gui_package.ppe_gui:main',
-            'dummy_ppe = gui_package.dummy_ppe_status:main',
             'experimental_gui = gui_package.experimental_ppe_gui:main',
+            'dummy_ppe = gui_package.dummy_ppe_status:main',
         ],
     },
 )
