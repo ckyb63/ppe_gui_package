d7e2aa2750b95bc1c04d41495d62f13acd8e35ef
ckyb63 <ckyb63@gmail.com>
2025-02-22 02:45:49 +0000

    Update 0.9.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ package_name = 'gui_package'
 
 setup(
     name=package_name,
-    version='0.8.3',
+    version='0.9.0',
     packages=find_packages(),
     data_files=[
         # Explicitly specify the marker file installation
